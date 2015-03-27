284fbfff67cde7b309bcc5aeac137ad61a87906c
diegobrocchi <dbrocchi@gmail.com>
2015-03-27 18:38:19 +0100

    Signed-off-by: diegobrocchi <dbrocchi@gmail.com>

--- Heat/Migrations/Configuration.vb
+++ Heat/Migrations/Configuration.vb
@@ -2,15 +2,18 @@ Imports System
 Imports System.Data.Entity
 Imports System.Data.Entity.Migrations
 Imports System.Linq
+Imports Microsoft.AspNet.Identity
+Imports Microsoft.AspNet.Identity.EntityFramework
 
 Namespace Migrations
 
-    Friend NotInheritable Class Configuration 
+    Friend NotInheritable Class Configuration
         Inherits DbMigrationsConfiguration(Of HeatDBContext)
 
         Public Sub New()
             AutomaticMigrationsEnabled = True
-            ContextKey = "Heat.HeatDBContext"
+            'ContextKey = "Heat.HeatDBContext"
+            ContextKey = "Heat"
         End Sub
 
         Protected Overrides Sub Seed(context As HeatDBContext)
@@ -24,6 +27,22 @@ Namespace Migrations
             '       New Customer() With {.FullName = "Andrew Peters"},
             '       New Customer() With {.FullName = "Brice Lambson"},
             '       New Customer() With {.FullName = "Rowan Miller"})
+
+            Dim manager As New UserManager(Of ApplicationUser)(New UserStore(Of ApplicationUser)(New ApplicationDbContext))
+
+            Dim demo As New ApplicationUser
+            demo.UserName = "demo"
+
+            Dim admin As New ApplicationUser
+            admin.UserName = "admin"
+
+            Dim user As New ApplicationUser
+            user.UserName = "user"
+
+            manager.Create(demo, "demo")
+            manager.Create(admin, "admin")
+            manager.Create(user, "user")
+
         End Sub
 
     End Class
