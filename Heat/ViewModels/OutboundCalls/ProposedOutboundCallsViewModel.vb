bdc324841f9284788c234a4bb7c4f02c01f6fc24
diegobrocchi <dbrocchi@gmail.com>
2015-10-27 07:29:16 +0100

    add viewmodels

---
+++ Heat/ViewModels/OutboundCalls/ProposedOutboundCallsViewModel.vb
@@ -0,0 +1,15 @@
+﻿Namespace ViewModels.OutboundCall
+
+    Public Class ProposedOutboundCallsViewModel
+
+        Property ID As Integer
+
+        Property User As String
+        Property Name As String
+        Property PlantID As Integer
+        Property PlantDetails As String
+        Property Telephone1 As String
+        Property Telephone2 As String
+
+    End Class
+End Namespace
---
+++ Heat/ViewModels/Products/IndexDataTableProductViewModel.vb
@@ -0,0 +1,11 @@
+﻿Namespace ViewModels.Product
+
+    Public Class IndexDataTableProductViewModel
+
+        Property ID As Integer
+        Property SKU As String
+        Property Description As String
+        Property UnitPrice As Decimal
+
+    End Class
+End Namespace
